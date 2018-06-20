429dabf06785f6fbf13bbbcbbe5b5fc7513ab9f3
al3xand3r-K <alexander.khristich@gmail.com>
2018-06-20 17:00:41 +0300

    add data faker

--- src/test/java/tests/AuthTest.java
+++ src/test/java/tests/AuthTest.java
@@ -2,6 +2,7 @@ package tests;
 
 import base.BaseTest;
 import com.codeborne.selenide.Configuration;
+import com.github.javafaker.Faker;
 import domain.User;
 import io.github.bonigarcia.wdm.ChromeDriverManager;
 import org.testng.annotations.BeforeClass;
@@ -13,24 +14,29 @@ import static com.codeborne.selenide.Condition.visible;
 
 public class AuthTest extends BaseTest {
 
+    private Faker f = new Faker();
+    private User registeredUser;
+
     @Test(enabled = false)
     public void canSignUpAsValidUser() {
-        User user = new User("John", "Doe",
-                            genEmail(), "78qa22",
-                            "514 S. Magnolia St.", "Orlando", "Florida", "32806",
-                            "16546546549");
+        User user = new User(f.name().firstName(), f.name().lastName(),
+                f.internet().emailAddress(), f.internet().password(),
+                f.address().streetAddress(), f.address().city(), f.address().state(), f.address().zipCode(),
+                f.phoneNumber().cellPhone());
+
         new LoginPage()
             .open()
             .signUpAs(user)
             .myWishlistsBtn.shouldBe(visible);
+
+        registeredUser = user;
     }
 
     @Test
     public void canSignInAsRegisteredUser() {
-        User user = new User("qatest2278@gmail.com", "78qa22");
         new LoginPage()
             .open()
-            .loginAs(user)
+            .loginAs(registeredUser)
             .myWishlistsBtn.shouldBe(visible);
     }
 
